d69bde6c888aa887d8326760087b9cd80419326e
sulugruk <77075122560@yandex.com>
2016-12-19 19:09:57 +0600

    Renamed

---
+++ ch2.1-2-3.helloworld.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# http://www.tldp.org/LDP/Bash-Beginners-Guide/html/sect_02_01.html
+# http://www.tldp.org/LDP/Bash-Beginners-Guide/html/sect_02_02.html
+# http://www.tldp.org/LDP/Bash-Beginners-Guide/html/sect_02_03.html
+# Don't forget to enable syntax highlighting `:sy enable`
+
+clear
+echo "The script starts now."
+
+echo "Hi, $USER!"
+echo
+
+echo "I will now fetch you a list of connected users:"
+echo
+#set -x
+w
+#set +x
+echo
+
+echo "I'm setting two variables now."
+COLOUR="black"
+VALUE="9"
+echo "This is a string: $COLOUR"
+echo "And this is a number: $VALUE"
+echo
+
+echo "I'm giving you back your prompt now."
+echo
+
+exit 0
