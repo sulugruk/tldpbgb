fd9ff12fc9368fe427259a89d82f8bdc307df580
sulugruk <77075122560@yandex.com>
2016-12-21 19:56:27 +0600

    Done

--- ch3.2.6.variable2.sh
+++ ch3.2.6.variable2.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
 # http://www.tldp.org/LDP/Bash-Beginners-Guide/html/sect_03_02.html
-# Don't forget to enable syntax highlighting
+# Don't forget to enable syntax highlighting in vim - `:sy enable`
 
-# This is backup of srcipt dir (need check .git tree)
-
-backupdir=~/bashlearn
-backupfiles=~/tldp
-tarfile=$tempdir/tldpbgb.tar
-bzipfile=/
-server=192.168.122.145
-remotedir=/home/kurgulus/
-logfile=script.log
-username=kurgulus
+# This script makes backup of srcipt dir (need check .git tree)
+# Change the values of the variables to make the script work for you:
 
 cd /tmp
+# I dont want to forget this command, thus using it.
 export tempdirb=`mktemp -d`
-cd $backupdir
-tar cf $tempdirb/tldpbgb.tar $backupfiles > /dev/null 2>&1
-echo "Backup stored to $tempdirb"
+backupdir=~/bashlearn
+backupfiles=tldpbgb
+tarfile=tldpbgb.tar
+server=192.168.122.145
+remotedir=/home/kurgulus
+logfile=ch3.2.6.variable2.sh.log
+username=kurgulus
 
-scp $tarfile $username@$server:$remotedir
-date >> ~/tldp/ch3.2.6.variable.log
-echo backup succeeded >> $remotedir/$logfile
+cd $backupdir
+
+# creating archive
+tar cf $tempdirb/$tarfile $backupfiles > /dev/null 2>&1
+echo "Backup stored to $tempdirb"
+cd $tempdir
+
+# copy the file to remote host via scp 
+#scp $tempdirb/$tarfile $username@$server:$remotedir
+
+# adding current date to log file 
+date >> $backupdir/$backupfiles/$logfile
+scp $tempdirb/$tarfile $backupdir/$backupfiles/$logfile $username@$server:$remotedir
+echo "backup succeeded >> $backupdir/$backupfiles/$logfile"
+
+# cleaning tmp dir
+rm -rf $tempdirb
+exit 0
