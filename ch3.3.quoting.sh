9559c0fd39e10729188e89e01d4d7b682ed14013
sulugruk <77075122560@yandex.com>
2016-12-23 10:57:15 +0600

    Done

---
+++ ch3.3.quoting.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+printf '# date=20161221 \n'
+date=20161221
+printf '\n'
+
+printf '# echo $date \n'
+echo $date
+printf '\n'
+
+printf '# echo \date \n'
+echo \date
+printf '\n'
+
+printf "# echo \'\$date\' \n"
+echo '$date'
+printf '\n'
+
+printf "# echo \'\\\$date\' \n"
+echo '\$date'
+printf '\n'
+
+printf "# echo \"\$date\" \n"
+echo "$date"
+printf '\n'
+
+printf "# echo \"\\\$date\" \n"
+echo "\$date"
+printf '\n'
+
+printf "# echo \"\`date\`\"  \n"
+echo "`date`"
+printf '\n'
+
+
+# echo "$'date'"
+echo -e "test \v test"
+
+
+
+# this is an error
+#echo "\`date`"
+
+#printf "# echo \'blah \'blah\' \n"
+#echo 'blah 'blah''
+#printf '\n'
+
+#printf "#echo \'blah \'\\blah\'\' \n"
+#echo 'blah '\blah''
+#printf '\n'
+
+#echo '\\'
+#printf '\n'
+
+#echo "\\"
+#printf '\n'
+
+
