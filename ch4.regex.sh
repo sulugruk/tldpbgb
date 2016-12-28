c730a3d9aa3f1784752f37df074d989072a6cfc1
sulugruk <77075122560@yandex.com>
2016-12-28 14:05:17 +0600

    Done

--- ch4.regex.sh
+++ ch4.regex.sh
@@ -13,15 +13,18 @@ printf '1. Display a list of all the users on your system who log in with the Ba
 printf "# cat /etc/passwd | grep /bash \n"
 cat /etc/passwd | grep /bash
 echo
+echo
 
 printf "2. From the /etc/group directory, display all lines starting with the string \"daemon\" \n"
 printf "# grep ^daemon /etc/group \n"
 grep ^daemon /etc/group
 echo
+echo
 
 printf "3. Print all the lines from the same file that don't contain the string. \n"
 printf "# grep -v daemon /etc/group \n"
 grep -v daemon /etc/group
+echo
 echo
 
 printf "4. Display localhost information from the /etc/hosts file, display the line number(s) matching the search string and count the number of occurrences of the string. \n"
@@ -33,23 +36,49 @@ echo
 printf "# number of occurrences \n"
 grep -cw `hostname` /etc/hosts
 echo
+echo
 
-printf "#5. Display a list of /usr/share/doc subdirectories containing information about shells. \n"
+printf "5. Display a list of /usr/share/doc subdirectories containing information about shells. \n"
 printf "# grep -nR1 'shells' /usr/share/doc/* \n"
 # grep -nr1 'shells' /usr/share/doc/*
 # or 
 # grep -nrH 'shells' /usr/share/doc/*
 grep -nR1 'shells' /usr/share/doc/*
 echo 
-
-printf "6. How many README files do these subdirectories contain? Don't count anything in the form of \"README.a_string\"."
-printf "# grep -Rn 'README' --include=README /usr/share/doc/*"
-grep -rn --include=README 'README' /usr/share/doc/*
 echo
 
-printf "7. Make a list of files in your home directory that were changed less that 10 hours ago, using grep, but leave out directories."
-ls -lR ~ | grep -E "`date '+%b %d '`[`date '+%H' --date='10 hours ago'``date '+%H'`]"
+printf "6. How many README files do these subdirectories contain? Don't count anything in the form of \"README.a_string\". \n"
+printf "# grep -Rn 'README' --include=README /usr/share/doc/* \n"
+grep -rn --include=README 'README' /usr/share/doc/*
+echo
+echo
 
+printf "7. Make a list of files in your home directory that were changed less that 10 hours ago, using grep, but leave out directories.\n"
+ls -lR ~ | grep -E "`date '+%b %d '`[`date '+%H' --date='10 hours ago'``date '+%H'`]" | grep -v '^d'
+echo
+echo
+
+printf "9. Can you find an alternative for wc -l, using grep? \n"
+# Example: ls -l | wc -l
+ls -l | grep -cE "`printf '\n'`"
+echo
+echo
+
+printf "10. Using the file system table (/etc/fstab for instance), list local disk devices. \n"
+#grep "/dev/[sd..]" /etc/fstab
+grep "/dev/[sh][[:alpha:]][[:alpha:]][[:digit:]]" /etc/fstab
+echo
+echo
+
+printf "11. Make a script that checks whether a user exists in /etc/passwd. For now, you can specify the user name in the script, you don't have to work with arguments and conditionals at this stage. \n"
+username='root'
+printf "# grep 'root' /etc/passwd \n"
+grep "$username" /etc/passwd
+echo
+echo
+
+printf "11. Display configuration files in /etc that contain numbers in their names. \n"
+ls -o /etc/ | grep "[a-Z][0-9]"
+echo
 
 ) | less
-
